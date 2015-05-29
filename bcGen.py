b2fccc674887959867dcff54561110c9ae86385a
johaywood <josh.haywood1@gmail.com>
2015-05-29 13:05:22 -0400

    working copy

--- bcGen.py
+++ bcGen.py
@@ -1,16 +1,14 @@
 from hubarcode.datamatrix import DataMatrixEncoder
 import sys, os
-import openpyxl
+from openpyxl import load_workbook
 from PIL import Image
 
-encoder = DataMatrixEncoder("HuDoRa")
-encoder.save( "test.png" )
-
-
 subjects = []
+study_num = sys.argv[1]
+subj_list_file = sys.argv[2]
 
 class Subject:
-  
+
   def __init__(self, subj_id, subj_num, barcode):
     self.subj_id = subj_id
     self.subj_num = subj_num
@@ -18,36 +16,69 @@ class Subject:
     self.barcode_color = self.get_barcode_color(barcode)
     self.barcode_path = self.get_barcode_path(barcode)
     self.barcode_img = self.get_barcode_image(barcode)
+    self.add_colored_border(self.barcode_path)
     subjects.append(self)
-    
-  def get_barcode_color(barcode):
-    color = ''
-    num = int(barcode[:-4])
-    if num < 2000:
-      color = "Yellow"
-      color_rgba = (255, 255, 0, 255)
-    if num < 4000:
-      color = "Cream"
-      color_rgba = (255, 229, 204, 255)
-    if num < 6000:
-      color = "Orange"
-      color_rgba = (255, 128, 0, 255)
-    if num < 8000:
-      color = "Blue"
-      color_rgba = (0, 0, 255, 255)
-    if num < 10000:
-      color = "Green"
-      color_rgba = (0, 225, 0, 255)
-    return color, color_rgba
-  
+
+  def get_barcode_color(self, barcode):
+    color_rgb = ''
+    let = str(self.barcode[:2])
+    num = int(self.barcode[-4:])
+    if let < 'FF':
+      color_rgb = (255, 255, 0)
+    elif let == 'FF' and num < 2000:
+      color_rgb = (255, 255, 0)
+    elif let < 'KK':
+      color_rgb = (255, 229, 204)
+    elif let == 'KK' and num < 4000:
+      color_rgb = (255, 229, 204)
+    elif let < 'PP':
+      color_rgb = (255, 128, 0)
+    elif let == 'PP' and num < 6000:
+      color_rgb = (255, 128, 0)
+    elif let < 'UU':
+      color_rgb = (0, 0, 255)
+    elif let == 'UU' and num < 8000:
+      color_rgb = (0, 0, 255)
+    elif let < 'ZZ':
+      color_rgb = (0, 225, 0)
+    elif let == 'ZZ' and num < 10000:
+      color_rgb = (0, 225, 0)
+    return color_rgb
+
   def get_barcode_path(self, barcode):
     cwd = os.getcwd()
-    path = cwd + '/' + barcode + '.png'
+    path = cwd + '\\' + study_num + '\\' + barcode + '.png'
     return path
-  
+
   def get_barcode_image(self, barcode):
     barcode_img = DataMatrixEncoder(barcode)
-    barcode_img.save("%s.png" % barcode)
+    barcode_img.save(self.barcode_path)
     return barcode_img
-  
-  def add_colored_border(barcode):
+
+  def add_colored_border(self, barcode):
+    img = Image.open(barcode)
+    img = img.convert('RGB')
+    px = img.load()
+    for x in range(img.size[0]):
+      for y in range(img.size[1]):
+        if px[x, y] == (150, 150, 150):
+          px[x, y] = self.barcode_color
+    img.save(self.barcode_path)
+
+def add_subjects(subj_list):
+  ensure_dir('%s/' % study_num)
+  wb = load_workbook(filename = subj_list)
+  ws = wb.active
+  num_subjects = ws.max_row
+  for x in range(0, num_subjects - 1):
+    Subject(ws.cell('A%s' % (x + 2)).value, ws.cell('B%s' % (x + 2)).value, ws.cell('C%s' % (x + 2)).value)
+    ws.cell('D%s' % (x + 2)).value = subjects[x].barcode_path
+  wb.save(subj_list)
+
+def ensure_dir(f):
+    d = os.path.dirname(f)
+    if not os.path.exists(d):
+        os.makedirs(d)
+
+add_subjects(subj_list_file)
+    
